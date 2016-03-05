0128d47a0ee2bad51e6baa5f171ee632d60cc69b
Xavier Lepretre <xavier.lepretre@gmail.com>
2016-03-05 14:27:43 +0000

    Hardcoded values in ShareIssuer.

--- contracts/ShareIssuer.sol
+++ contracts/ShareIssuer.sol
@@ -1,5 +1,6 @@
-contract ShareIssuer {
-	address issuer;
+import "Owned";
+
+contract ShareIssuer is Owned {
 	address authorisedSettler;
 
 	mapping (address => uint256) sharesOwned;
@@ -7,38 +8,48 @@ contract ShareIssuer {
 
 	event OnSharesIssued(uint256 count);
 	event OnSharesTransfered(address from, address to, uint256 count);
-
-    modifier fromIssuer() {
-        if (msg.sender != issuer) {
-            throw;
-        }
-        _
-    }
+	event OnAuthorisedSettlerChanged(address newAuthorisedSettler);
 
     modifier fromAuthorisedSettler() {
-        if (msg.sender != issuer) {
+        if (msg.sender != authorisedSettler) {
             throw;
         }
         _
     }
 
 	function ShareIssuer() {
-		issuer = msg.sender;
-		sharesOwned[issuer] = 100;
+		sharesOwned[owner] = 100;
+		shareholders.push(owner);
 	}
 
-	function issueShares(uint256 quantity) 
-		fromIssuer() {
-		sharesOwned[issuer] += quantity;
-		OnSharesIssued(quantity);
+	function getAuthorisedSettler() constant
+		returns (address) {
+		return authorisedSettler;
 	}
 
 	function registerAuthorisedSettler(address newAuthorisedSettler) 
-		fromIssuer() {
+		fromOwner() {
 		authorisedSettler = newAuthorisedSettler;
+		OnAuthorisedSettlerChanged(newAuthorisedSettler);
 	}
 
-	function transferSharesTo(address from, address to, uint256 count) 
+	function getShareholders() constant
+		returns (address[]) {
+		return shareholders;		
+	}
+
+	function getShareholding(address shareholder) constant
+		returns (uint256) {
+		return sharesOwned[shareholder];
+	}
+
+	function issueShares(uint256 quantity) 
+		fromOwner() {
+		sharesOwned[owner] += quantity;
+		OnSharesIssued(quantity);
+	}
+
+	function transferShares(address from, address to, uint256 count) 
 		fromAuthorisedSettler(){
 		if (sharesOwned[from] < count) {
 			throw;
